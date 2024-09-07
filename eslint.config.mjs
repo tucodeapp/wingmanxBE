90e1032b1417dd03be1a6ac6400c721b14e5d6fc
Arturas Tumenas <arturastumenas@Arturass-MacBook-Pro.local>
2024-09-07 19:23:24 +0300

    Heroku

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
 
-export default [
+module.exports = [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
