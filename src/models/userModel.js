c2c1d648769d12f6d5eaf2c5a11089ba104e6050
Arturas <Arturas@Netzets-MBP.lan>
2024-09-07 23:04:23 +0300

    Heroku

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,81 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const subscriptionSchema = new Schema({
+  originalTransactionId: {
+    type: String,
+  },
+  autoRenewProductId: {
+    type: String,
+  },
+  productId: {
+    type: String,
+  },
+  autoRenewStatus: {
+    type: Number,
+  },
+  renewalPrice: {
+    type: Number,
+  },
+  currency: {
+    type: String,
+  },
+  signedDate: {
+    type: Number,
+  },
+  environment: {
+    type: String,
+  },
+  recentSubscriptionStartDate: {
+    type: Number,
+  },
+  renewalDate: {
+    type: Number,
+  },
+  transactionId: {
+    type: String,
+  },
+  webOrderLineItemId: {
+    type: String,
+  },
+  bundleId: {
+    type: String,
+  },
+  subscriptionGroupIdentifier: {
+    type: String,
+  },
+  purchaseDate: {
+    type: Number,
+  },
+  originalPurchaseDate: {
+    type: Number,
+  },
+  expiresDate: {
+    type: Number,
+  },
+  quantity: {
+    type: Number,
+  },
+  type: {
+    type: String,
+  },
+  inAppOwnershipType: {
+    type: String,
+  },
+  transactionReason: {
+    type: String,
+  },
+  storefront: {
+    type: String,
+  },
+  storefrontId: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+});
+
 const userSchema = new Schema(
   {
     name: {
@@ -9,20 +84,16 @@ const userSchema = new Schema(
     },
     email: {
       type: String,
-      required: [true, "Email is required "],
+      required: [true, "Email is required"],
       unique: true,
     },
     password: {
       type: String,
       required: [true, "Password is required"],
     },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
     subscription: {
-      type: Array,
-      default: [],
+      type: subscriptionSchema,
+      default: {},
     },
   },
   {
@@ -30,8 +101,8 @@ const userSchema = new Schema(
   }
 );
 
-const UserSchema = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = {
-  UserSchema,
+  User,
 };
