0184e4ace52ed9c89e3058220618701a532199d6
Arturas <Arturas@Netzets-MBP.lan>
2024-09-07 23:36:48 +0300

    Heroku

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -48,8 +48,8 @@ const userSchema = new Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+const UserSchema = mongoose.model("User", userSchema);
 
 module.exports = {
-  User,
+  UserSchema,
 };
