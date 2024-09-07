5e2e446b7a1887d1fd7280050f7bbfbda7dd382c
Arturas <Arturas@Netzets-MBP.lan>
2024-09-07 23:35:01 +0300

    Heroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,6 +6,8 @@ const { UserSchema: User } = require("../models/userModel");
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
+  console.log(req.body);
+
   if (!name || !email || !password) {
     return res.status(400).json({
       error: "Please add all necessary information!",
