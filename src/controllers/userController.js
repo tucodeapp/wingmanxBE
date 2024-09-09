832f324b60382c3f7627ec13e47ed6b4c72c4022
Arturas <Arturas@Netzets-MacBook-Pro.local>
2024-09-09 10:48:00 +0300

    Heroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -93,13 +93,13 @@ const receiveNotifications = async (req, res) => {
 
     res.status(200).send("Notification received");
 
-    const decodedData = base64.decode(notificationData.message.data);
+    const decodedData = base64.decode(notificationData?.message.data);
 
     const jsonData = JSON.parse(decodedData);
     console.log(jsonData, "dataaa");
     console.log("RECEIVED NOTIF!!!!");
 
-    const decoded = jwt.decode(notificationData.signedPayload, {
+    const decoded = jwt.decode(notificationData?.signedPayload, {
       complete: true,
     });
 
