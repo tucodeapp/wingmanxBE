fcb14295fdcead2e58c9f1083be0205c2ac94b06
Arturas <Arturas@Netzets-MacBook-Pro.local>
2024-09-23 10:44:12 +0300

    Heroku

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const {
-  sendNotification,
-  getNotifications,
-  registerNotification,
-  updateNotification,
-} = require("../controllers/notificationController");
+// const express = require("express");
+// const {
+//   sendNotification,
+//   getNotifications,
+//   registerNotification,
+//   updateNotification,
+// } = require("../controllers/notificationController");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/send", sendNotification);
+// router.post("/send", sendNotification);
 
-router
-  .route("/")
-  .get(getNotifications)
-  .post(registerNotification)
-  .patch(updateNotification);
+// router
+//   .route("/")
+//   .get(getNotifications)
+//   .post(registerNotification)
+//   .patch(updateNotification);
 
-module.exports = router;
+// module.exports = router;
